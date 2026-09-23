dad1aedb48a9cd7dc041393c00223566140a1f7a
raymondmarechu07-ui <raymondmarechu07@gmail.com>
2026-09-23 14:58:16 +0300

    fix: deploy iframe header worker with DTrader build

--- packages/core/build/config.js
+++ packages/core/build/config.js
@@ -65,6 +65,7 @@ const copyConfig = base => {
         },
         { from: path.resolve(__dirname, '../src/root_files/robots.txt'), to: 'robots.txt', toType: 'file' },
         { from: path.resolve(__dirname, '../src/root_files/_headers'), to: '_headers', toType: 'file' },
+        { from: path.resolve(__dirname, '../src/root_files/_worker.js'), to: '_worker.js', toType: 'file' },
         { from: path.resolve(__dirname, '../src/root_files/sitemap.xml'), to: 'sitemap.xml', toType: 'file' },
         {
             from: path.resolve(__dirname, '../src/public/images/favicons/favicon.ico'),
